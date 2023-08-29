ed94977524ec2ec609d0e3108fb8837872be548c
Ching0810 <neinchin123@gmail.com>
2023-08-29 18:24:32 +0800

    build: add sumbit form and basic router

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import express from "express"
+import express, { urlencoded } from "express"
 import { engine } from "express-handlebars"
 const app = express()
 const port = 3000
@@ -6,6 +6,8 @@ const port = 3000
 app.engine('.hbs', engine({extname: '.hbs'}))
 app.set('view engine', '.hbs')
 app.set('views', './views')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.redirect('/random_password_generator')
@@ -15,6 +17,11 @@ app.get('/random_password_generator', (req, res) => {
   res.render('home')
 })
 
+app.post('/random_password_generator/result', (req, res) => {
+  const checkBox = req.body
+  console.log(checkBox)
+})
+
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
+  console.log(`Random password generator app listening on port http://localhost:${port}`)
 })
