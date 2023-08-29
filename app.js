38bda0b55797c6005fae0df3d6c663d29b7df400
Ching0810 <neinchin123@gmail.com>
2023-08-29 16:04:37 +0800

    build: add static file and set template engine

--- app.js
+++ app.js
@@ -3,8 +3,16 @@ import { engine } from "express-handlebars"
 const app = express()
 const port = 3000
 
+app.engine('.hbs', engine({extname: '.hbs'}))
+app.set('view engine', '.hbs')
+app.set('views', './views')
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.redirect('/random_password_generator')
+})
+
+app.get('/random_password_generator', (req, res) => {
+  res.render('home')
 })
 
 app.listen(port, () => {
