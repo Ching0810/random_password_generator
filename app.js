d1c1417fdd42564f748a4e6b97784f6ac81e0a44
Ching0810 <neinchin123@gmail.com>
2023-08-30 17:49:18 +0800

    build: add password generator function according to option checkbox

--- app.js
+++ app.js
@@ -17,9 +17,32 @@ app.get('/random_password_generator', (req, res) => {
   res.render('home')
 })
 
+const charCodes = new Map();
+charCodes.set('lowercaseCheckbox', 'abcdefghijklmnopqrstuvwxyz');
+charCodes.set('uppercaseCheckbox', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+charCodes.set('numberCheckbox', '0123456789');
+charCodes.set('symbolCheckbox', '!#$%&*+-<=>?@^_~');
+
+function createPassword (totalChar, passwordLength) {
+  let str = ''
+  for (let i = 0; i < passwordLength; i++) {
+    str += totalChar[Math.floor(Math.random() * totalChar.length)].toString()
+  }
+  return str
+}
+
 app.post('/random_password_generator/result', (req, res) => {
   const checkBox = req.body
-  console.log(checkBox)
+  const passwordLength = req.body.passwordLength
+  let totalChar = ''
+  charCodes.forEach((value, key) => {
+    if (Object.keys(checkBox).find((element) => element === key)) {
+      totalChar += value
+      console.log(totalChar)
+    }
+  })
+  const password = createPassword(totalChar, passwordLength)
+  res.render('result', { password })
 })
 
 app.listen(port, () => {
---
+++ public/javascripts/index.js
@@ -0,0 +1,5 @@
+const charCodes = new Map();
+charCodes.set('lowercase', 'abcdefghijklmnopqrstuvwxyz');
+charCodes.set('uppercase', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+charCodes.set('number', '0123456789');
+charCodes.set('symbol', '!#$%&*+-<=>?@^_~');
